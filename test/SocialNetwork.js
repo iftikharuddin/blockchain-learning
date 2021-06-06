0532415f85dfd399ffb8d1ac18bb6d6a48823a4b
Iftikhar uddin <iftikhar@connextar.com>
2021-06-07 00:59:45 +0500

    Wrote some test code before deploying to blockchain

---
+++ test/SocialNetwork.js
@@ -0,0 +1,30 @@
+const SocialNetwork = artifacts.require("./SocialNetwork.sol");
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('SocialNetwork', (accounts) => {
+    let socialNetwork;
+
+    before(async () => {
+        socialNetwork = await SocialNetwork.deployed()
+    })
+
+    describe('deployment', async () => {
+        // test if deploys successfull
+        it('deploys successfully', async () => {
+            const address = await socialNetwork.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+
+        it('has a name', async () => {
+            const name = await socialNetwork.name()
+            assert.equal(name, "Social Network")
+        })
+    })
+
+})
